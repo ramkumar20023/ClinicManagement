4de1de200fc35160e413b9d4c86329b60fe3416a
Ramkumar <ramkumar.mdev@gmail.com>
2025-04-04 15:38:20 +0530

    updated

--- ReceptionistApp/views.py
+++ ReceptionistApp/views.py
@@ -6,20 +6,21 @@ from django.utils.dateparse import parse_date
 from django.utils.timezone import now
 from .models import PatientDetails, Appointment, AppointmentBill
 from .serializers import PatientSerializer, AppointmentSerializer, AppointmentBillSerializer
+from Clinicapp.permissions import IsReceptionist,IsAdmin
 
 # Patient Management Views
 class PatientListCreateView(generics.ListCreateAPIView):
     queryset = PatientDetails.objects.all()
     serializer_class = PatientSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | IsReceptionist]
 
 class PatientDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = PatientDetails.objects.all()
     serializer_class = PatientSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | IsReceptionist]
 
 class DeactivatePatientView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | IsReceptionist]
     
     def patch(self, request, pk):
         try:
@@ -34,15 +35,15 @@ class DeactivatePatientView(APIView):
 class AppointmentListCreateView(generics.ListCreateAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | IsReceptionist]
 
 class AppointmentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | IsReceptionist]
 
 class CancelAppointmentView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | IsReceptionist]
     
     def patch(self, request, pk):
         try:
@@ -55,7 +56,7 @@ class CancelAppointmentView(APIView):
 
 # New View: List Appointments by Date
 class AppointmentsByDateView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | IsReceptionist]
 
     def get(self, request):
         appointment_date = request.query_params.get('date')
@@ -76,7 +77,7 @@ class AppointmentsByDateView(APIView):
 # Appointment Listing Views
 class AppointmentsByPatientView(generics.ListAPIView):
     serializer_class = AppointmentSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | IsReceptionist]
 
     def get_queryset(self):
         patient_id = self.kwargs['patientId']
@@ -84,7 +85,7 @@ class AppointmentsByPatientView(generics.ListAPIView):
 
 class AppointmentsByDoctorView(generics.ListAPIView):
     serializer_class = AppointmentSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | IsReceptionist]
 
     def get_queryset(self):
         doctor_id = self.kwargs['doctorId']
@@ -92,7 +93,7 @@ class AppointmentsByDoctorView(generics.ListAPIView):
 
 class AppointmentsByStatusView(generics.ListAPIView):
     serializer_class = AppointmentSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | IsReceptionist]
 
     def get_queryset(self):
         status_param = self.request.query_params.get('status')
@@ -102,16 +103,16 @@ class AppointmentsByStatusView(generics.ListAPIView):
 class AppointmentBillListCreateView(generics.ListCreateAPIView):
     queryset = AppointmentBill.objects.all()
     serializer_class = AppointmentBillSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | IsReceptionist]
 
 class AppointmentBillDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = AppointmentBill.objects.all()
     serializer_class = AppointmentBillSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | IsReceptionist]
 
 # New View: List Bills by Date Range
 class BillsByDateRangeView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated | IsReceptionist]
 
     def get(self, request):
         start_date = request.query_params.get('startDate')
